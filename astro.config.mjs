3b4e60357844b85371578d096a65d95c77a585fa
tsots <mosalatsotetsi@gmail.com>
2022-10-12 06:12:03 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+  site: "https://git-katalist.github.io/",
+  base: "/maintain",
+});
